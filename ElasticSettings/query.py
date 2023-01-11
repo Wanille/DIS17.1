790279b4c440b9db2779f0f032815dc0f2e7020c
Nils Grote <nilsgrote1403@gmail.com>
2023-01-11 20:00:12 +0100

    added score_function

--- ElasticSettings/query.py
+++ ElasticSettings/query.py
@@ -1,16 +1,29 @@
 def get_query(q: str):
     query = {
-        "query": {
-            "multi_match": {
-                    "query": q,
-                    "type": "cross_fields",
-                    "fields": [
-                        "source_x",
-                        "title^3",
-                        "abstract^2",
-                        "journal",
-                        "authors"
-                ]}
+        "query": { 
+            "function_score": {
+                    "query": {
+                        "multi_match": {
+                            "query": q,
+                            "type": "cross_fields",
+                            "fields": [
+                                "source_x",
+                                "title^3",
+                                "abstract^2",
+                                "journal",
+                                "authors"
+                                ]}},
+                    "gauss": {
+                        "publish_time": {
+                            "origin": "2021-12-31",
+                            "scale": "20m",
+                            "offset": "3m",
+                            "decay": 0.3
+                        }
+                    }
+                  },
+            
+                
         }
     }
     return query
